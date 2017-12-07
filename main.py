36fcedae53eec7cab9b95997c3be781d852716eb
Haider Ali Punjabi <haiderali@cyberservices.com>
2017-12-07 20:37:13 +0530

    [ci skip] Update

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ mg = materialgenerator.MaterialGenerator()
 qg = quotegenerator.QuoteGenerator()
 caption = "Material Quote of the Day #QOTD #MaterialDesign #Quotes #Quote"
 quote = qg.get_quote()
-image = mg.draw_image(quote['quoteText'], quote['quoteAuthor'], (400, 400))
+image = mg.draw_image(quote['quoteText'], quote['quoteAuthor'], (4000, 4000))
 image.save(os.path.join(dir,'image.png'),'PNG')
 twitter.upload_status(caption,open(os.path.join(dir,'image.png'),'rb'))
